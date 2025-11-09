7e9a31eb44623ac99da201dfe422748fb2bfe668
Duc Dang <ducdang@Mac.lan>
2025-11-09 23:35:45 +0700

    LC 739 & starting 853

--- src/main/java/com/zenbox/leetcode/LC739DailyTemperatures.java
+++ src/main/java/com/zenbox/leetcode/LC739DailyTemperatures.java
@@ -1,5 +1,9 @@
 package com.zenbox.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * {@code LC739DailyTemperatures}
  * <p>
@@ -8,20 +12,51 @@ package com.zenbox.leetcode;
  */
 public class LC739DailyTemperatures {
 
-    public int[] dailyTemperaturesWithStack(int[] temperatures) {
-        int[] result = new int[temperatures.length];
+    public int[] dailyTemperaturesWithStackImprove(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>(); // pair: [temp, index]
 
-        for(int i = 0; i < temperatures.length; i++) {
-            int temp = temperatures[i];
-            for(int j = i + 1; j < temperatures.length; j++) {
-                int tempFuture = temperatures[j];
-                if(tempFuture > temp) {
-                    result[i] = j - i;
-                    break;
-                }
+        for (int i = 0; i < temperatures.length; i++) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && t > stack.peek()[0]) {
+                int[] pair = stack.pop();
+                res[pair[1]] = i - pair[1];
             }
+            stack.push(new int[]{t, i});
         }
+        return res;
+    }
 
+    /**
+     * Time: O(n)
+     * Space: O(n)
+     * @param temperatures
+     * @return
+     */
+    public int[] dailyTemperaturesWithStackAndMap(int[] temperatures) {
+        int n = temperatures.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[n];
+
+        stack.push(temperatures[n - 1]);
+        map.put(temperatures[n - 1], n - 1);
+
+        for(int i = n - 2; i >= 0; i--) {
+            Integer maxTemp = stack.peek();
+            int curr = temperatures[i];
+            while(maxTemp != null && curr >= maxTemp) {
+                map.remove(maxTemp);
+                stack.pop();
+                maxTemp = stack.isEmpty() ? null : stack.peek();
+            }
+            if(maxTemp != null && curr < maxTemp) {
+                int indexMaxTemp = map.get(maxTemp);
+                result[i] = indexMaxTemp - i;
+            }
+            stack.push(curr);
+            map.put(curr, i);
+        }
         return result;
     }
 
