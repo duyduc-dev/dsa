4b59a6cc68db186a348d9a722421faf2885db3ad
duyduc-dev <dangduyducdangduyduc@gmail.com>
2026-07-09 07:23:01 +0700

    LC 1834

--- src/main/java/com/zenbox/leetcode/LC1834SingleThreadedCPU.java
+++ src/main/java/com/zenbox/leetcode/LC1834SingleThreadedCPU.java
@@ -1,5 +1,8 @@
 package com.zenbox.leetcode;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * {@code LC1834SingleThreadedCPU}
  * <p>
@@ -11,7 +14,47 @@ package com.zenbox.leetcode;
 public class LC1834SingleThreadedCPU {
   class Solution {
     public int[] getOrder(int[][] tasks) {
-      return new int[] {};
+      int n = tasks.length;
+      int[][] sortedTasks = new int[n][3];
+
+      for (int i = 0; i < n; i++) {
+        sortedTasks[i] = new int[] { tasks[i][0], tasks[i][1], i };
+      }
+
+      Arrays.sort(sortedTasks, (a, b) -> a[0] != b[0]
+          ? Integer.compare(a[0], b[0])
+          : Integer.compare(a[2], b[2]));
+
+      PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] != b[0]
+          ? Integer.compare(a[0], b[0])
+          : Integer.compare(a[1], b[1]));
+
+      int[] res = new int[n];
+
+      int idxRes = 0;
+      int i = 0;
+      int t = 0;
+
+      while (idxRes < n) {
+        while (i < n) {
+          int[] task = sortedTasks[i];
+          if (task[0] <= t) {
+            q.add(new int[] { task[1], task[2] });
+            i++;
+          } else
+            break;
+        }
+
+        if (!q.isEmpty()) {
+          int[] task = q.poll();
+          res[idxRes++] = task[1];
+          t += task[0];
+        } else {
+          t = sortedTasks[i][0];
+        }
+      }
+
+      return res;
     }
   }
 }
