00371630bab21f812fd8c2578a9b0766372f6cc3
duyduc-dev <dangduyducdangduyduc@gmail.com>
2026-07-21 06:41:05 +0700

    LC 1578

--- src/main/java/com/zenbox/leetcode/LC1578MinimumTimeToMakeRopeColorful.java
+++ src/main/java/com/zenbox/leetcode/LC1578MinimumTimeToMakeRopeColorful.java
@@ -4,26 +4,26 @@ package com.zenbox.leetcode;
  * {@code LC1834SingleThreadedCPU}
  * <p>
  * Solution for
- * <a href="https://leetcode.com/problems/single-threaded-cpu/">1834.
- * Single-Threaded CPU</a>.
+ * <a href=
+ * "https://leetcode.com/problems/minimum-time-to-make-rope-colorful">1578.
+ * Minimum Time to Make Rope Colorful</a>.
  * </p>
  */
 public class LC1578MinimumTimeToMakeRopeColorful {
     class Solution {
         public int minCost(String colors, int[] neededTime) {
-            int l = 0, r = 0;
-            int n = neededTime.length;
-            int res = 0;
+            int l = 0, r = 0, n = colors.length(), res = 0, max = 0;
 
             while (l <= r && r < n) {
-                int maxTime = 0;
-                while (l <= r && r < n && colors.charAt(l) == colors.charAt(r)) {
+                max = 0;
+
+                while (r < n && colors.charAt(l) == colors.charAt(r)) {
                     res += neededTime[r];
-                    maxTime = Math.max(maxTime, neededTime[r]);
+                    max = Math.max(max, neededTime[r]);
                     r++;
                 }
 
-                res -= maxTime;
+                res -= max;
                 l = r;
             }
 
