66b0c440929b783dd6f18d1f9d48819816f3c7c3
EXTERNAL Dang Duy Duc (AMARIS, MS/EET) <external.DuyDuc.Dang@bcn.bosch.com>
2026-09-15 11:49:05 +0700

    LC125

--- src/main/java/com/zenbox/leetcode/LC125ValidPalindrome.java
+++ src/main/java/com/zenbox/leetcode/LC125ValidPalindrome.java
@@ -3,14 +3,53 @@ package com.zenbox.leetcode;
 /**
  * {@code LC125ValidPalindrome}
  * <p>
- * Solution for <a href="https://leetcode.com/problems/valid-palindrome">LeetCode Problem 125: Valid Palindrome</a>.
+ * Solution for
+ * <a href="https://leetcode.com/problems/valid-palindrome">LeetCode Problem
+ * 125: Valid Palindrome</a>.
  * </p>
  */
 public class LC125ValidPalindrome {
 
+    class Solution {
+        final int MIN_CHAR = 97;
+        final int MAX_CHAR = 122;
+
+        public boolean isPalindrome(String s) {
+            int n = s.length();
+            int l = 0, r = n - 1;
+
+            if (n == 1)
+                return true;
+
+            s = s.toLowerCase();
+
+            while (l < r) {
+                while (!isCharValid(s.charAt(l)) && l < r)
+                    l++;
+                while (!isCharValid(s.charAt(r)) && l < r)
+                    r--;
+
+                if ((int) s.charAt(l) == (int) s.charAt(r)) {
+                    l++;
+                    r--;
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private boolean isCharValid(char s) {
+            int charInt = (int) s;
+            return (charInt >= 'a' && charInt <= 'z') || (charInt >= '0' && charInt <= '9');
+        }
+    }
+
     /**
      * Time O(n)
      * Space O(1)
+     * 
      * @param s
      * @return
      */
@@ -18,15 +57,18 @@ public class LC125ValidPalindrome {
         s = s.toLowerCase();
         int n = s.length();
         int l = 0, r = n - 1;
-        while(l < r) {
-            while(r > l & (s.charAt(r) - 'a' >= 26 || s.charAt(r) - 'a' < 0 ) && (s.charAt(r) - '0' > 9 || s.charAt(r) - '0' < 0)) {
+        while (l < r) {
+            while (r > l & (s.charAt(r) - 'a' >= 26 || s.charAt(r) - 'a' < 0)
+                    && (s.charAt(r) - '0' > 9 || s.charAt(r) - '0' < 0)) {
                 r--;
             }
-            while(l < r && (s.charAt(l) - 'a' > 26 || s.charAt(l) - 'a' < 0) && (s.charAt(l) - '0' > 9 || s.charAt(l) - '0' < 0)) {
+            while (l < r && (s.charAt(l) - 'a' > 26 || s.charAt(l) - 'a' < 0)
+                    && (s.charAt(l) - '0' > 9 || s.charAt(l) - '0' < 0)) {
                 l++;
             }
-            if(l > r) return false;
-            if(s.charAt(r) != s.charAt(l)) {
+            if (l > r)
+                return false;
+            if (s.charAt(r) != s.charAt(l)) {
                 return false;
             }
             r--;
